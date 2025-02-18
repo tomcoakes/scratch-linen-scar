6e36ec7e4c7b6521bffeeabd632f0fcb7f46e02a
Glitch (scratch-linen-scar) <none>
2025-02-18 14:13:43 +0000

    🌁🐽 Checkpoint ./src/pages/tracker.html:66277726/644 ./src/pages/tracker.js:66277726/8598

--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -1,19 +1,21 @@
 // src/pages/tracker.js
 
-// --- Import React ---
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// --- Import React with explicit CDN paths and NAMED imports (WORKAROUND) ---
+import * as React from 'https://unpkg.com/react@18/umd/react.development.js';
+import * as ReactDOM from 'https://unpkg.com/react-dom@18/umd/react-dom.development.js';
 
 // --- Main App Component ---
 function App() {
     return (
-        <div>
-            <h1>Hello React Tracker!</h1> 
-            {/* We'll put our order table component here soon */}
-        </div>
+        React.createElement('div', null, // <-- Correct React.createElement syntax
+            React.createElement('h1', null, 'Hello React Tracker!') // <-- Correct React.createElement syntax
+        )
     );
 }
 
 // --- Render the App ---
 const root = ReactDOM.createRoot(document.getElementById('order-table-root')); // Target the div in tracker.html
-root.render(<App />);
+root.render(React.createElement(App)); // <-- NO CHANGE HERE (this line was already correct)
+
+// --- MODIFY THIS LINE BELOW - Access createRoot using bracket notation: ---
+ReactDOM["createRoot"](document.getElementById('order-table-root')).render(React.createElement(App)); 
